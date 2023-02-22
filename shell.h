9b16ecbbc8c32076bcec8299f627ee7786508072
hummingjay <jays7mill@gmail.com>
2023-02-23 00:32:38 +0300

    rewritten shell.h

--- shell.h
+++ shell.h
@@ -6,12 +6,11 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
-/*#include <sys/wait.h>*/
+#include <sys/wait.h>
 #include <sys/stat.h>
 #include <limits.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <stddef.h>
 
 /* for read/write buffers */
 #define READ_BUF_SIZE 1024
